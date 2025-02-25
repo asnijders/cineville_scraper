ab59a05337443441a09020bfd7bad119387dcbad
Ard Snijders <ardsnijders@gmail.com>
2025-02-25 15:38:11 +0100

    file deletion, download sh update

--- scripts/download_movies_dataset.sh
+++ scripts/download_movies_dataset.sh
@@ -5,4 +5,5 @@ mkdir -p "$(dirname "$0")/../data"
 
 # Download the file into the data directory
 curl -L -o "$(dirname "$0")/../data/the-movies-dataset.zip" \
-  https://www.kaggle.com/api/v1/datasets/download/rounakbanik/the-movies-dataset
+    https://grouplens.org/datasets/movielens/32m/
+#   https://www.kaggle.com/api/v1/datasets/download/rounakbanik/the-movies-dataset
