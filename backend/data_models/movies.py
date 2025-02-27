b450946fa61d33f25e978b1037235244cbd64055
Ard Snijders <ardsnijders@gmail.com>
2025-02-27 01:19:19 +0100

    fix: make movies data model nullable

--- backend/data_models/movies.py
+++ backend/data_models/movies.py
@@ -7,20 +7,21 @@ class Movie(Base):
 
     movie_id = Column(String, primary_key=True)  # Hash ID
     title = Column(String, nullable=False)
-    year = Column(Integer)
-    movie_link = Column(String, unique=True)  # Filmladder movie URL
-    imdb_link = Column(String, unique=True)  # IMDb URL
-    imdb_year = Column(Integer)
-    rating = Column(Float)
-    genres = Column(JSON)  # List of genres
-    content_rating = Column(String)
-    duration = Column(String)  # ISO 8601 format (e.g., PT1H32M)
-    director = Column(JSON)  # List of directors
-    writers = Column(JSON)  # List of writers
-    actors = Column(JSON)  # List of actors
-    rating_count = Column(Integer)
-    plot = Column(String)
-    release_date = Column(Date)
-    keywords = Column(JSON)  # List of keywords
-    poster_url = Column(String)
-    trailer_url = Column(String)
+    year = Column(Integer, nullable=True)  # Allow NULL
+    movie_link = Column(String, unique=True, nullable=False)  # Filmladder movie URL
+    imdb_link = Column(String, unique=True, nullable=True)  # IMDb URL
+    imdb_year = Column(Integer, nullable=True)
+    rating = Column(Float, nullable=True)
+    genres = Column(JSON, nullable=True)  # List of genres
+    content_rating = Column(String, nullable=True)
+    duration = Column(String, nullable=True)  # ISO 8601 format (e.g., PT1H32M)
+    director = Column(JSON, nullable=True)  # List of directors
+    writers = Column(JSON, nullable=True)  # List of writers
+    actors = Column(JSON, nullable=True)  # List of actors
+    rating_count = Column(Integer, nullable=True)
+    plot = Column(String, nullable=True)
+    release_date = Column(Date, nullable=True)
+    keywords = Column(JSON, nullable=True)  # List of keywords
+    poster_url = Column(String, nullable=True)
+    trailer_url = Column(String, nullable=True)
+
