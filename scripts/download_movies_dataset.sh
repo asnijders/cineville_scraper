327a44e97336e427ef42b1cee561f52ea21be02e
Ard Snijders <ardsnijders@gmail.com>
2025-03-11 09:22:35 +0100

    chores: (repo): clean up old code

--- scripts/download_movies_dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ensure the data directory exists
-mkdir -p "$(dirname "$0")/../data"
-
-# Download the file into the data directory
-curl -L -o "$(dirname "$0")/../data/the-movies-dataset.zip" \
-    https://grouplens.org/datasets/movielens/32m/
-#   https://www.kaggle.com/api/v1/datasets/download/rounakbanik/the-movies-dataset
